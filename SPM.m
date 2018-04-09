2f4247a128acdc3c56d88b69334da3f3fda3112f
profpaulsson <opaulsson92@gmail.com>
2018-04-09 13:39:20 +0200

    new

--- SPM.m
+++ SPM.m
@@ -12,31 +12,31 @@ k = [ksigma./sigma];
 N = (epsr*cosd(theta)+sqrt(epsr-sind(theta).^2)).^2;
 T = sind(theta).^2-epsr.*(1+sind(theta).^2);
 avv = (epsr-1).*(T./N);
-W = ElfouhailyFun(k);
+W1 = ElfouhailyFun(2.*k(1).*sind(theta)); 
+W2 = ElfouhailyFun(2.*k(2).*sind(theta));
+W3 = ElfouhailyFun(2.*k(3).*sind(theta));
 
-rcs = 10*log10((8.*k.^4.*sigma^2).*cosd(theta).^4.*(abs(avv).^2).*W)
-
-for i = 1:1:length(U19)
-nrcs(:,i) = rcs(kl(i),l(i),theta,avv(theta));
-end
-
-nrcsPM = rcsPM(sigma(1),K,L(1),avv(theta)); 
+rcs1 = 10*log10((8.*k(1).^4.*sigma(1)^2).*cosd(theta).^4.*(abs(avv).^2).*W1)
+rcs2 = 10*log10((8.*k(1).^4.*sigma(1)^2).*cosd(theta).^4.*(abs(avv).^2).*W2)
+rcs3 = 10*log10((8.*k(1).^4.*sigma(1)^2).*cosd(theta).^4.*(abs(avv).^2).*W3)
 
 figure(1)
 hold on
 subplot(2,2,1)
-plot(theta,nrcs(:,1),theta,nrcs(:,2),theta,nrcs(:,3))
-title('Small pertubations model, Gaussian surface spectra');
-ylabel('NRCS (dB)');
-xlabel('Angle of incidence (degree)');
-legend('k \sigma = 0.1, k_l = 1','k \sigma = 0.2, k_l = 2','k \sigma = 0.3, k_l = 3');
+plot(theta,rcs1(1,:),theta,rcs1(2,:),theta,rcs1(3,:))
+ylim([-75 0])
 grid on
-
 subplot(2,2,2)
-plot(theta,nrcsPM)
-title('Small pertubations model, Pierson-Moskowits surface spectra');
+plot(theta,rcs2(1,:),theta,rcs2(2,:),theta,rcs2(3,:))
+ylim([-75 0])
+grid on
+subplot(2,2,3)
+plot(theta,rcs3(1,:),theta,rcs3(2,:),theta,rcs3(3,:))
+title('Small pertubations model, Elfouhaily surface spectra');
 ylabel('NRCS (dB)');
 xlabel('Angle of incidence (degree)');
+legend('U_1_0 = 5m/s','U_1_0 = 10 m/s','U_1_5 = 15 m/s');
+ylim([-75 0])
 grid on
 
 %subplot(2,2,3)
